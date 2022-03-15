57c07c706fb9de50c52ceea5accaba41bf0a8f4b
KICKASS21 <aharjai21@gmail.com>
2022-03-15 23:59:29 +0530

    Create pizza, fish-chips, chicken-pakora, webpages

--- convert-recipe-to-list.py
+++ convert-recipe-to-list.py
@@ -1,23 +1,38 @@
-recipe = '''12 uncooked lasagna noodles
-1 pound sweet Italian sausage
-⅔ cup chopped onions
-½ tablespoon minced garlic
-⅔ cup chopped fresh parsley, divided
-3 (6 ounce) cans tomato paste
-1 (15 ounce) can tomato sauce
-2 cups water
-1 ½ teaspoons Italian seasoning
-1 ½ teaspoons dried oregano
-1 ½ teaspoons dried basil leaves
-1 pound part-skim ricotta cheese
-1 (10 ounce) package chopped spinach, thawed and squeeze dried
-½ cup grated Parmesan cheese
-3 eggs
-2 teaspoons garlic salt
-¼ teaspoon ground black pepper
-3 cups shredded mozzarella cheese'''
+recipe = '''½ (12 ounce) can CONTADINA® Tomato Paste
 
-res = recipe.split(sep="\n")
+1 teaspoon dried oregano, crushed
+
+1 teaspoon dried basil, crushed
+
+½ teaspoon garlic powder
+
+½ teaspoon onion powder
+
+½ teaspoon sugar
+
+½ teaspoon salt
+
+¼ teaspoon black pepper
+
+3 ¼ cups all-purpose flour, or more as needed
+
+2 (.25 ounce) envelopes FLEISCHMANN'S® Pizza Crust Yeast or RapidRise® Yeast
+
+1 tablespoon sugar
+
+1 ½ teaspoons salt
+
+1 ⅓ cups very warm water (120 degrees F to 130 degrees F)
+
+⅓ cup oil
+
+1 (6 ounce) package HORMEL® Pepperoni
+
+1 cup shredded mozzarella cheese, or more to taste'''
+
+res = recipe.split(sep="\n\n")
 # print(res)
+
 for a in res:
+    # print(a)
     print("<li>" + a + "</li>")
